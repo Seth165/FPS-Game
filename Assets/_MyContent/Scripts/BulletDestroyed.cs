6c722a3442f2e56bc2af78afbe5a9603d6996a20
Seth165 <64886052+Seth165@users.noreply.github.com>
2020-09-29 20:13:18 +1000

    bullet scripting and prefab modifications
    - modified the enemy prefab to have a health bar
    - started writing a script to destroy bullets on impact
    - added heakth bar to enemy

---
+++ Assets/_MyContent/Scripts/BulletDestroyed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroyed : MonoBehaviour
+{
+    // destroyd bullet on collision with enemy
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
