11ffe77647da7306dca3b9cf7b5770d7aacf023c
Seth165 <64886052+Seth165@users.noreply.github.com>
2020-10-15 20:40:33 +1000

    Set up a Main Menu annd added materials to floors and death zone
    - Added a main menu scene
    - Added button layout to Main Menu
    - Added 3d materials to floors and death zone
    - added Script to kill the player when walking out of bounds
    - Added Game Manager to scene
    - Added more files to neaten things up
    - added a game manager to Main Game Scene

---
+++ Assets/_MyContent/Scripts/Manager/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public HighScores HighScores;
+
+    public GameObject MainMenuImage;
+
+    public Button NewGameButton;
+
+    public GameObject[] Player;
+
+    private float gameTime = 0f;
+    public float GameTime { get { return gameTime; } }
+    public enum GameState
+    {
+        Start,
+        Playing,
+        GameOver
+    };
+
+    private GameState gState;
+    public GameState State { get { return gState; } }
+
+    private void Awake()
+    {
+        gState = GameState.Start;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
