6100a4beeca49397451a868705daad1a355bf73c
Seth165 <64886052+Seth165@users.noreply.github.com>
2020-11-03 16:28:04 +1000

    Finihing touched to game
    - textured 3d models for 3 different greyboxing assets
    - edited texturing to floors and walls
    - added four pillars
    - added two bins
    - added a rug?

---
+++ Assets/_MyContent/Scripts/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+    public GameObject playerHealthUI;
+    public Slider slider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < maxHealth)
+        {
+
+        }
+    }
+}
